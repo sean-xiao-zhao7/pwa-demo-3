7edf1006e5cdca6297dafccc8abc6fb70d756f67
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2022-08-09 14:38:55 -0400

    Add deferred add home screen

--- public/service-worker.js
+++ public/service-worker.js
@@ -3,3 +3,7 @@ self.addEventListener("install", (event) => {});
 self.addEventListener("activate", (event) => {
     return self.clients.claim();
 });
+
+self.addEventListener("fetch", (event) => {
+    event.respondWith(fetch(event.request));
+});
--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,3 +1,11 @@
 if ("serviceWorker" in navigator) {
     navigator.serviceWorker.register("/service-worker.js").then(() => {});
 }
+
+let deferredPrompt;
+
+window.addEventListener("beforeinstallprompt", (event) => {
+    event.preventDefault();
+    deferredPrompt = event;
+    return false;
+});
