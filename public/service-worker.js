ebea722c3d3b990c96a768425dd062afd0dd36f4
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2022-08-10 11:35:22 -0400

    Use pre-caching

--- public/service-worker.js
+++
@@ -1,9 +0,0 @@
-self.addEventListener("install", (event) => {});
-
-self.addEventListener("activate", (event) => {
-    return self.clients.claim();
-});
-
-self.addEventListener("fetch", (event) => {
-    event.respondWith(fetch(event.request));
-});
---
+++ public/sw.js
@@ -0,0 +1,37 @@
+self.addEventListener("install", function (event) {
+    event.waitUntil(
+        caches.open("static").then((cache) => {
+            cache.addAll([
+                "/",
+                "/index.html",
+                "/src/js/app.js",
+                "/src/js/feed.js",
+                "/src/js/promise.js",
+                "/src/js/fetch.js",
+                "/src/js/material.min.js",
+                "/src/css/app.css",
+                "/src/css/feed.css",
+                "/src/images/main-image.jpg",
+                "https://fonts.googleapis.com/css?family=Roboto:400,700",
+                "https://fonts.googleapis.com/icon?family=Material+Icons",
+                "https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css",
+            ]);
+        })
+    );
+});
+
+self.addEventListener("activate", function (event) {
+    return self.clients.claim();
+});
+
+self.addEventListener("fetch", function (event) {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            if (response) {
+                return response;
+            } else {
+                return fetch(event.request);
+            }
+        })
+    );
+});
